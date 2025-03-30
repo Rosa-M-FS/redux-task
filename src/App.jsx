22b0492ec67455c3433a6950b5565adec2b2af1c
Rosa-M-FS <rmlopezrobles@gmail.com>
2025-03-30 18:52:33 +0200

    Ejercicio Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, deleteTodo } from './redux/todosSlice';
+import { useState } from 'react';
+
 const App = () => {
+  const todos = useSelector(state => state.todos);
+  const dispatch = useDispatch();
+  const [input, setInput] = useState('');
+
+  const handleAdd = () => {
+    if (input.trim()) {
+      dispatch(addTodo(input));
+      setInput('');
+    }
+  };
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(id));
+  };
   
   return (
-    <></>
+    <>
+      <div className='app'>
+        <h1>Lista de Tareas</h1>
+          <input
+            type="text"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            onKeyDown={e=>e.key==='Enter'&& handleAdd()}
+            placeholder="Nueva tarea"
+          />
+          <button onClick={handleAdd}>Agregar</button>
+
+        <p>Total de tareas: {todos.length}</p>
+        <ul className='task-list'>
+          {todos.map(todo => (
+            <li key={todo.id} className='task-item'>
+              {todo.text}
+              <button onClick={() => handleDelete(todo.id)}>Eliminar</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
---
+++ src/redux/todosSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.push({ id: Date.now(), text: action.payload });
+    },
+    deleteTodo: (state, action) => {
+      return state.filter(todo => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
